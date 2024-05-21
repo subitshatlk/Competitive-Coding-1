77a352eb9e51f8d0ecc8706b73e1de6bb8c5d1f8
Subitsha <subitsha@Subitshas-MacBook-Air.local>
2024-05-21 20:36:31 +0530

    Completed interview problems 1 and 2

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,42 @@
-
+//Time Complexity - O(log n)
+// Space Complexity - O(1)
+// Able to code in interview - Yes
+//We initially check the middle element and if the previous element or next element to mid is not consecutive, then we return the missing element. 
+// If not, check the diff between the value at mid and the index mid , if the difference is not 1, then we know that the sorted array has some missing
+//element in the first half. Else in the second half. 
+class Sample{
+    public static int getMissingElement(int[] arr){
+        int low = 0;
+        int high = (arr.length) - 1;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2 ; 
+            System.out.println(mid);
+    
+            if(arr[0] != 1){
+                return 1;
+            }
+            if(mid != 0 && arr[mid] - arr[mid - 1] != 1){
+                
+                return arr[mid] - 1;   
+            }
+            else if(mid != arr.length -1 && arr[mid + 1] - arr[mid] != 1){
+                return arr[mid] + 1;  
+            }
+            else if(arr[mid] - mid != 1){
+                high = mid - 1;
+    
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return arr.length + 1;
+    }
+    
+    public static void main(String args[]){
+        int[] arr = {2,3,4,5};
+        int missingElement = getMissingElement(arr);
+        System.out.println("The missing element:" +missingElement);
+    }
+    }
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,137 @@
+//Time Complexity - O(log n) - insert
+// O(log n) - remove()  - minHeapify takes O(log n) 
+// extract minimum - O(1)
+//Space - O(n)
+class MinHeap{
+ 
+    private int[] Heap;
+    private int size;
+    private int maxsize;
+    private static final int FRONT = 1;
+ 
+  
+    public MinHeap(int maxsize)
+    {
+ 
+        this.maxsize = maxsize;
+        this.size = 0;
+ 
+        Heap = new int[this.maxsize + 1];
+        Heap[0] = Integer.MIN_VALUE;
+    }
+ 
 
+    private int parent(int pos) { return pos / 2; }
+ 
+    private int leftChild(int pos) { return (2 * pos); }
+ 
+    private int rightChild(int pos)
+    {
+        return (2 * pos) + 1;
+    }
+ 
+    private boolean isLeaf(int pos)
+    {
+ 
+        if (pos > (size / 2)) {
+            return true;
+        }
+ 
+        return false;
+    }
+ 
+    private void swap(int first, int second)
+    {
+ 
+        int temp;
+        temp = Heap[first];
+ 
+        Heap[first] = Heap[second];
+        Heap[second] = temp;
+    }
+ 
+
+   private void minHeapify(int pos)
+   {      
+     if(!isLeaf(pos)){
+       int swapPos= pos;
+       if(rightChild(pos)<=size)
+          swapPos = Heap[leftChild(pos)]<Heap[rightChild(pos)]?leftChild(pos):rightChild(pos);
+       else
+         swapPos= leftChild(pos);
+        
+       if(Heap[pos]>Heap[leftChild(pos)] || Heap[pos]> Heap[rightChild(pos)]){
+         swap(pos,swapPos);
+         minHeapify(swapPos);
+       }
+        
+     }       
+   }
+ 
+    public void insert(int element)
+    {
+ 
+        if (size >= maxsize) {
+            return;
+        }
+ 
+        Heap[++size] = element;
+        int current = size;
+ 
+        while (Heap[current] < Heap[parent(current)]) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+ 
+    public void print()
+    {
+        for (int i = 1; i <= size / 2; i++) {
+ 
+            System.out.print(
+                " PARENT : " + Heap[i]
+                + " LEFT CHILD : " + Heap[2 * i]
+                + " RIGHT CHILD :" + Heap[2 * i + 1]);
+ 
+            
+            System.out.println();
+        }
+    }
+ 
+  
+    public int remove()
+    {
+ 
+        int popped = Heap[FRONT];
+        Heap[FRONT] = Heap[size--];
+        minHeapify(FRONT);
+ 
+        return popped;
+    }
+ 
+    
+    public static void main(String[] arg)
+    {
+ 
+       
+        System.out.println("The Min Heap is ");
+    
+        MinHeap minHeap = new MinHeap(15);
+ 
+        
+        minHeap.insert(5);
+        minHeap.insert(3);
+        minHeap.insert(17);
+        minHeap.insert(10);
+        minHeap.insert(84);
+        minHeap.insert(19);
+        minHeap.insert(6);
+        minHeap.insert(22);
+        minHeap.insert(9);
+ 
+        minHeap.print();
+ 
+        System.out.println("The Minimum val is "
+                           + minHeap.remove());
+    }
+}
